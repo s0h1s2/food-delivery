9d28e15d840cea4b9828da955d18d8deb41cbfbf
shkar <shkarhsardar@gmail.com>
2024-07-12 17:57:43 +0300

    integerated stripe checkout from backend

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -28,14 +28,62 @@ const createOrderCheckoutSession = async (req: Request, res: Response) => {
       throw new Error("Restuarant not found.")
     }
     const lineItems = createLineItems(checkoutSessionRequest, restuarant.menuItems)
-
+    const session = await createSession(lineItems, "TEST_ORDER_ID", restuarant.deliveryPrice, restuarant._id.toString())
+    if (!session.url) {
+      return res.status(500).send()
+    }
   } catch (error) {
     Logger.error(error)
-    return res.status(StatusCodes.SERVICE_UNAVAILABLE).send({ message: error.raw.message })
+    return res.status(StatusCodes.SERVICE_UNAVAILABLE).send()
   }
 }
-const createLineItems = (checkout: CheckoutSessionRequest, menuItems: IMenuItem[]) => { }
+const createLineItems = (checkout: CheckoutSessionRequest, menuItems: IMenuItem[]) => {
+  const lineItems = checkout.cartItems.map((cart) => {
+    const menuItem = menuItems.find((item) => item._id.toString() === cart.menuItemId.toString())
+    if (!menuItem) {
+      throw new Error(`Menu item not found:${menuItem}`)
+    }
+    const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
+      price_data: {
+        currency: 'usd',
+        unit_amount: menuItem.price,
+        product_data: {
+          name: menuItem.name
+        }
+      },
+      quantity: parseInt(cart.quantity)
+    }
+    return line_item
+  })
+  return lineItems
+}
 
 export default {
   createOrderCheckoutSession
 }
+const createSession = async (lineItems: Stripe.Checkout.SessionCreateParams.LineItem[], orderId: string, deliveryPrice: number, restuarantId: string) => {
+  const sessionData = await STRIPE.checkout.sessions.create({
+    line_items: lineItems,
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          display_name: "Delivery",
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: deliveryPrice,
+            currency: 'usd'
+          }
+        }
+      }
+    ],
+    mode: "payment",
+    metadata: {
+      orderId,
+      restuarantId
+    },
+    success_url: `${FRONTEND_URL}/order-status?success=true`,
+    cancel_url: `${FRONTEND_URL}/detail/${restuarantId}?cancelled=true`,
+  })
+  return sessionData
+}
+
